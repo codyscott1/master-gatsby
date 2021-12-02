e6a5c88e8e3fa8f4c0748c1bb86d769ef9feaabb
codyscott1 <codywaukee@gmail.com>
2021-12-02 15:32:56 -0600

    remove red background

--- starter-files/gatsby/gatsby-browser.js
+++ starter-files/gatsby/gatsby-browser.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
  
-import './src/styles/red.css';
-
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
