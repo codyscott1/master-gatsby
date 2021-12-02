703b763d56d11cca3b616eb7c329a34c49c5ad32
codyscott1 <codywaukee@gmail.com>
2021-12-02 16:20:39 -0600

    start layout css

--- starter-files/gatsby/src/components/Nav.js
+++ starter-files/gatsby/src/components/Nav.js
@@ -1,9 +1,50 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  .logo {
+    transform: translateY(-25%);
+  }
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    text-align: center;
+    list-style: none;
+    align-items: center;
+    margin: top;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(1) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot now</Link>
@@ -12,7 +53,9 @@ export default function Nav() {
           <Link to="/pizzas">Pizza Menu</Link>
         </li>
         <li>
-          <Link to="/">LOGO</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">SliceMasters</Link>
@@ -21,6 +64,6 @@ export default function Nav() {
           <Link to="/orders">Order Ahead!</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
