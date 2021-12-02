ed9edcf77647171341c9804b87f594840fbb6e8f
codyscott1 <codywaukee@gmail.com>
2021-12-02 15:32:30 -0600

    temporarily add red background

--- starter-files/gatsby/gatsby-browser.js
+++ starter-files/gatsby/gatsby-browser.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+ 
+import './src/styles/red.css';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
