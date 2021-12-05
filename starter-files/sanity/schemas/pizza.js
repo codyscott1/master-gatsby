2e1366f3a73fa7079282a85d4dfedf80abf64b4d
codyscott1 <codywaukee@gmail.com>
2021-12-05 15:33:32 -0600

    add ability to add toppings with preview within pizza view

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -37,5 +37,30 @@ export default {
       validation: (Rule) => Rule.min(1000).max(50000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const toppingDisplay = Object.values(toppings).filter(Boolean).join(', ');
+
+      return {
+        title,
+        media,
+        subtitle: toppingDisplay,
+      };
+    },
+  },
 };
---
+++ starter-files/sanity/schemas/toppings.js
@@ -0,0 +1,31 @@
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping name',
+      type: 'string',
+      description: 'What is the name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is the topping vegetarian',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? `🍂` : ''}`,
+    }),
+  },
+};
